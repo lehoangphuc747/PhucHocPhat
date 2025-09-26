4a4a26a01105f6cdd7b127d28230e93403c8d9b5
[dyad] <lphc664@yahoo.com.vn>
2025-09-26 22:09:53 +0900

    [dyad] Hoàn thành component Sheet và khắc phục các lỗi TypeScript còn lại - wrote 3 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,20 +2,25 @@
 
 import * as React from "react";
 import { GripVertical } from "lucide-react";
-import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "react-resizable-panels";
+import {
+  PanelGroup, // Đã sửa tên import
+  Panel, // Đã sửa tên import
+  PanelResizeHandle, // Đã sửa tên import
+} from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 
-const ResizablePanelGroupComponent = ResizablePanelGroup;
-const ResizablePanelComponent = ResizablePanel;
+// Định nghĩa các thành phần với tên theo shadcn/ui
+const ResizablePanelGroup = PanelGroup;
+const ResizablePanel = Panel;
 
-const ResizableHandleComponent = React.forwardRef<
-  React.ElementRef<typeof ResizableHandle>,
-  React.ComponentPropsWithoutRef<typeof ResizableHandle> & {
+const ResizableHandle = React.forwardRef<
+  React.ElementRef<typeof PanelResizeHandle>, // Cập nhật kiểu tham chiếu
+  React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
     withHandle?: boolean;
   }
 >(({ withHandle, className, ...props }, ref) => (
-  <ResizableHandle
+  <PanelResizeHandle // Sử dụng thành phần đã import đúng tên
     ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0",
@@ -28,12 +33,12 @@ const ResizableHandleComponent = React.forwardRef<
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizableHandle>
+  </PanelResizeHandle>
 ));
-ResizableHandleComponent.displayName = ResizableHandle.displayName;
+ResizableHandle.displayName = "ResizableHandle"; // Cập nhật displayName
 
 export {
-  ResizablePanelGroupComponent as ResizablePanelGroup,
-  ResizablePanelComponent as ResizablePanel,
-  ResizableHandleComponent as ResizableHandle,
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
 };
