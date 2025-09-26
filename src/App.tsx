a011a7bb72ce3d31552d15730eaf5f294238a837
[dyad] <lphc664@yahoo.com.vn>
2025-09-26 22:17:52 +0900

    [dyad] Khắc phục lỗi "React.Children.only" bằng cách bọc các component con của QueryClientProvider trong Fragment - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,29 @@ import NotFound from "./pages/NotFound";
 import Library from "./pages/Library";
 import ArticleDetail from "./pages/ArticleDetail";
 import Layout from "./components/Layout";
-import React from "react"; // Import React để sử dụng React.Fragment (nếu cần ở nơi khác)
+import React from "react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    {/* Global Toasters được đặt ở đây, bên ngoài TooltipProvider và BrowserRouter */}
-    <Toaster />
-    <SonnerToaster />
-    <TooltipProvider>
-      {/* BrowserRouter là phần tử con duy nhất của TooltipProvider */}
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/library/:id" element={<ArticleDetail />} />
-          </Route>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <React.Fragment> {/* Bọc tất cả các component con trong một Fragment */}
+      <Toaster />
+      <SonnerToaster />
+      <TooltipProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/library/:id" element={<ArticleDetail />} />
+            </Route>
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </React.Fragment>
   </QueryClientProvider>
 );
 
