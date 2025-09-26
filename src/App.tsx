acbec8de50727bd83106da7d25193d90538c2e93
[dyad] <lphc664@yahoo.com.vn>
2025-09-26 22:17:00 +0900

    [dyad] Khắc phục lỗi "React.Children.only" bằng cách bọc BrowserRouter trong Fragment - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -18,17 +18,19 @@ const App = () => (
     <Toaster />
     <SonnerToaster />
     <TooltipProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/library/:id" element={<ArticleDetail />} />
-          </Route>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <React.Fragment> {/* Bọc BrowserRouter trong một Fragment */}
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/library/:id" element={<ArticleDetail />} />
+            </Route>
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </React.Fragment>
     </TooltipProvider>
   </QueryClientProvider>
 );
